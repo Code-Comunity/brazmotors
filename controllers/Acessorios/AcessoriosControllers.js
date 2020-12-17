0aa8c03658b77b9318f489162bcac6ad8191a225
matias96321 <matias_69914081@hotmail.com>
2020-12-17 14:22:15 -0300

    Correções no controller de acessorios

--- controllers/Acessorios/AcessoriosControllers.js
+++ controllers/Acessorios/AcessoriosControllers.js
@@ -1,7 +1,9 @@
 const Acessorios = require('../../models/Acessorios/AcessoriosModule')
 module.exports = {
+
     async FindAll(Request,Response){
        const acessorios = await Acessorios.find({}).limit(10)
     return Response.json(acessorios)    
     }
+    
 }
