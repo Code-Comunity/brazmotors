64b1cab056a5196868b198a31d5c671ab3235025
Patrick Rocha <59058473+ptkm1@users.noreply.github.com>
2021-03-18 21:48:37 -0300

    Update MecanicaEspecializadaController.js

--- controllers/Mecanica_Especializada/MecanicaEspecializadaController.js
+++ controllers/Mecanica_Especializada/MecanicaEspecializadaController.js
@@ -1,11 +1,11 @@
 const MecanicaEspecializada = require('../../models/Mecanica_Especializada/MecanicaEspecializadaModel')
 module.exports = {
     async FindAll(Request,Response){
-       const mecanica = await MecanicaEspecializada.find({}).limit()
+       const mecanica = await MecanicaEspecializada.find({})
     return Response.json(mecanica)    
     },
     async FindById(Request,Response){
-            const mecanica = await MecanicaEspecializada.find({_id: Request.params.id}).limit(10)
+            const mecanica = await MecanicaEspecializada.find({_id: Request.params.id})
     return Response.json(mecanica)    
     }
 }
