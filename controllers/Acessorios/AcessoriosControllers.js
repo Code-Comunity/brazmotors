c358fc3021b01d3c8d4f10fd6bd728ab1b29d7d2
Patrick Rocha <59058473+ptkm1@users.noreply.github.com>
2021-03-18 21:44:29 -0300

    Update AcessoriosControllers.js

--- controllers/Acessorios/AcessoriosControllers.js
+++ controllers/Acessorios/AcessoriosControllers.js
@@ -2,7 +2,7 @@ const Acessorios = require('../../models/Acessorios/AcessoriosModel')
 module.exports = {
 
     async FindAll(Request,Response){
-       const acessorios = await Acessorios.find({}).limit(10)
+       const acessorios = await Acessorios.find({})
     return Response.json(acessorios)    
     }
     
