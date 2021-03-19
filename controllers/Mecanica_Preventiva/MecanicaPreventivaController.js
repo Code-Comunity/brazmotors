ce7708b1b69a8f2c2db7917cb87a00f57badd603
Patrick Rocha <59058473+ptkm1@users.noreply.github.com>
2021-03-18 21:49:00 -0300

    Update MecanicaPreventivaController.js

--- controllers/Mecanica_Preventiva/MecanicaPreventivaController.js
+++ controllers/Mecanica_Preventiva/MecanicaPreventivaController.js
@@ -1,11 +1,11 @@
 const MecanicaPreventiva = require('../../models/Mecanica_Preventiva/MecanicaPreventivaModel')
 module.exports = {
     async FindAll(Request,Response){
-       const mecanica = await MecanicaPreventiva.find({}).limit(10)
+       const mecanica = await MecanicaPreventiva.find({})
     return Response.json(mecanica)    
     },
     async FindById(Request,Response){
-        const mecanica = await MecanicaPreventiva.find({_id: Request.params.id}).limit(10)
+        const mecanica = await MecanicaPreventiva.find({_id: Request.params.id})
     return Response.json(mecanica)    
     }
 }
