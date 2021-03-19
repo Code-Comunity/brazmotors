6ed94d1098248d7b42b9287faed4492e0455e2c1
Patrick Rocha <59058473+ptkm1@users.noreply.github.com>
2021-03-18 21:42:58 -0300

    Update MarcasControllers.js

--- controllers/Marcas/MarcasControllers.js
+++ controllers/Marcas/MarcasControllers.js
@@ -5,12 +5,12 @@ const MarcasModelosViews = require('../../views/Marcas/MarcasModelosViews')
 
 module.exports = {
     async FindAll(Request,Response){
-       const marcas = await Marcas.find({}).limit(10)
+       const marcas = await Marcas.find({})
     return Response.json(MarcasViews.RenderAll(marcas))    
     },
   
     async FindById(Request,Response){
-        const Modelos = await Carros.find({marca: Request.params.id}).limit(10)
+        const Modelos = await Carros.find({marca: Request.params.id})
     return Response.json(await MarcasModelosViews.RenderAll(Modelos)
           
     
